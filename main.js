8efdbbf8ec35e5f1a4379f5f8cf2f6cf3e70931b
Robin Millette <robin@millette.info>
2019-04-20 16:27:16 -0400

    Register tags and init.

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 // npm
-import { register, mount } from "riot"
+import { component, register } from "riot"
+// import { component } from "riot"
 
 // self
 import RiotApp from "./tags/app.riot"
+import TagTwo from "./tags/tag-two.riot"
+import TagThree from "./tags/tag-three.riot"
+// import { RiotApp, registerAll } from "./tags"
+// registerAll()
 
-register("riot-app", RiotApp)
-mount("riot-app", { message: "Hello there" })
+register("tag-two", TagTwo)
+register("tag-three", TagThree)
+
+component(RiotApp)(document.querySelector("main"), {
+  message: "Hello there",
+})
