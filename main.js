6cec31f33778068c7a210057ab8566606174f7cd
Robin Millette <robin@millette.info>
2019-04-23 03:36:28 -0400

    Register max tag.

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ import RiotApp from "./tags/app.riot"
 import TagTwo from "./tags/tag-two.riot"
 import TagThree from "./tags/tag-three.riot"
 import Tags from "./tags/tags.riot"
+import Max from "./tags/max.riot"
 
+registerAuto(Max)
 registerAuto(TagTwo)
 registerAuto(Tags)
 const tags = registerAuto(TagThree)
